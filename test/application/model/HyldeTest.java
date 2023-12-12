4848ec5e16942ce82da9efa14afab61b0becae35
Martin <martinpugdal@live.dk>
2023-12-12 08:36:54 +0100

    Create HyldeTest.java

---
+++ test/application/model/HyldeTest.java
@@ -0,0 +1,12 @@
+package application.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyldeTest {
+
+    @Test
+    void addOpbevaring() {
+    }
+}
